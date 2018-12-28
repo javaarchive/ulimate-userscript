ec4c034bb2e8986adb78de59472873b91c995b78
Raymond <20248577+javaarchive@users.noreply.github.com>
2018-12-27 21:49:10 -0800

    Create script.user.js

---
+++ script.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Ultimate user script(With AoPS scripts, and more)
+// @namespace    http://tampermonkey.net/
+// @version      0.1.1
+// @description  Make web browsing easier!
+// @author       You
+// @match        https://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // Options
+    const normalset = true;
+    const reload_time = 1000;
+    console.log("==== Thank You for using the best user script in the world =====");
+    if(window.location.pathname.startsWith("/community/category-admin/") && window.location.hostname === "artofproblemsolving.com"){
+       setInterval(function(){
+           if ($("#blog-changer").length){return;}
+           $.ajax({ type: "GET",url: "https://acscdn.pythonanywhere.com/aopsblog.html",success : function(text){if ($("#blog-changer").length){return;}$('.cmty-category-admin').prepend('<div id = "blog-changer">'+text+'</div>');}
+    });},reload_time);
+       }else if(window.location.hostname === "artofproblemsolving.com"){
+       console.log("Ultimate user script is working on AoPS");
+       }
+
+})();
